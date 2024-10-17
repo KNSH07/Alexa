ae7780b6ccc7d71ea84af6bc7e1d77fb23fb137f
JEAN FREE FIRE Bradley <jeanbelout64@gmail.com>
2024-10-17 15:30:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "Zz1FVA6K#rjSUAREzhKwW7-yr3mncK5TQsikxSFjPt8Yz-hW1Ha8",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
